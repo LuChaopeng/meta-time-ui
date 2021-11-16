008a3765f409731cd9cd622e39df68a9c5ba3dac
luchaopeng <chaopeng.lu@qq.com>
2021-11-16 17:47:49 +0800

    解决跨域，成功与后端通信

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,96 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div v-if="hasCookie" class="main-page">
+      <meta-tag></meta-tag>
+    </div>
+    <div v-else class="login">
+      <div class="user mihray" @click="selectUser(users[0])">Mihray</div>
+      <div class="user ming" @click="selectUser(users[1])">Ming</div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MetaTag from "@/components/MetaTag"
+import axios from "axios";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MetaTag
+  },
+  data(){
+    return{
+      eventList:[],
+      users:['mihray','ming'],
+      hasCookie:false
+    }
+  },
+  methods:{
+    selectUser(name){
+      axios.post('/api/',{
+        userName:name
+      })
+          .then((res)=>{
+            if(res.data === 'success'){
+              this.hasCookie = true
+            }
+          })
+          .catch((err)=>{
+            console.log(err)
+          })
+    }
+  },
+  created() {
+    axios.post('/api/',{
+      req:'init'
+    })
+    .then((res)=>{
+      if(res.data !== 'select-user'){
+        this.hasCookie = true
+      }
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
   }
 }
 </script>
 
 <style>
 #app {
+
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+.login {
+  height: 100vh;
+  background-color: #efefef;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.user {
+  font-size: 3rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 3rem;
+  border-radius: 0.8rem;
+  color: #ffffff;
+  width: 66vw;
+  height: 5rem;
+  cursor: pointer;
+}
+.mihray{
+  background-color: darkorange;
+}
+.ming{
+  background-color: darkmagenta;
+}
+
 </style>
