f9bab292e68839e170074e39e61751d3b53e72e7
luchaopeng <chaopeng.lu@qq.com>
2022-01-25 19:23:27 +0800

    feat:开发/生产环境下使用不同baseURL 1、添加.env.development和.env.production配置文件； 2、在axios服务建立时根据环境使用不同baseURL，并允许携带cookie；

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,6 +1,11 @@
 import axios from 'axios'
 
+/**
+ * 根据环境变量修改baseURL，并携带cookie（axios请求默认不带cookie）
+ */
 const service = axios.create({
+  baseURL: process.env.NODE_ENV === 'dev' ? '/' : 'http://111.229.176.245:3000/',
+  withCredentials: true,
   timeout: 5000
 })
 
